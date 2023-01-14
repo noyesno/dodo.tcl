1fc7972adb2ff600209704da0412ab0e7faf3c36
Sean Zhang <noyesno.net@gmail.com>
2023-01-14 09:23:47 +0000

    add @usage and @doc

--- lib/dodo.tcl
+++ lib/dodo.tcl
@@ -1,6 +1,14 @@
 
 package provide dodo.tcl 0.1
 
+proc @doc {args} {
+  uplevel 1 [lrange $args 1 end]
+}
+
+proc @usage {args} {
+  uplevel 1 [lrange $args 1 end]
+}
+
 proc proc/n {name argv body} {
 
     proc [format "%s/%d" $name [llength $argv]] $argv $body
@@ -11,3 +19,32 @@ proc proc/n {name argv body} {
 
 }
 
+
+proc lfirst {aList} { lindex $aList 0 }
+proc lsecond {alist} { lindex $aList 1 }
+proc llast {aList} { lindex $aList end }
+
+
+@doc {
+  list comprehension
+} \
+proc lcomp {xExpr xVar xList {xFilter 1}} {
+    lmap $xVar $xList [join [list \
+            [list if $xFilter {} else continue] \
+	    [list expr $xExpr] \
+    ] "\n"]
+}
+
+
+proc infix {a cmdName b} {
+    uplevel 1 [list $cmdName $a $b]
+}
+
+
+proc lreduce {aList xVar resVar aExpr} {
+    set $resVar 0
+    foreach $xVar $aList {
+        set $resVar [expr $aExpr]
+    }
+    return [set $resVar]
+}
